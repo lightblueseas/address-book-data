a7b71afd6a7b224cb013c6ec62fd0996b4f7be1e
astrapi69 <asterios.raptis@gmx.net>
2015-12-10 17:29:35 +0100

    parent pom version update for extend from abstract rest client

--- address-book-rest-client/src/main/java/de/alpharogroup/address/book/rest/client/AddressBookRestClient.java
+++ address-book-rest-client/src/main/java/de/alpharogroup/address/book/rest/client/AddressBookRestClient.java
@@ -1,36 +1,19 @@
 package de.alpharogroup.address.book.rest.client;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.ws.rs.core.MediaType;
-
-import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
-import org.apache.cxf.jaxrs.client.WebClient;
-
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
-
 import de.alpharogroup.address.book.rest.api.AddressesResource;
 import de.alpharogroup.address.book.rest.api.CountriesResource;
 import de.alpharogroup.address.book.rest.api.FederalstatesResource;
 import de.alpharogroup.address.book.rest.api.ZipcodesResource;
+import de.alpharogroup.cxf.rest.client.AbstractRestClient;
 import lombok.Getter;
 
 /**
- * The class {@link AddressBookRestClient} is a rest client for accessing the rest services from the address-book database.
+ * The class {@link AddressBookRestClient} is a rest client for accessing the rest services from the
+ * address-book database.
  */
-public class AddressBookRestClient
+public class AddressBookRestClient extends AbstractRestClient
 {
 
-	/** The Constant DEFAULT_HTTP_PORT. */
-	public static final int DEFAULT_HTTP_PORT = 8080;
-
-	/** The Constant BASE_URL_PREFIX. */
-	public static final String BASE_URL_PREFIX = "http://localhost";
-
-	/** The Constant DEFAULT_BASE_URL. */
-	public static final String DEFAULT_BASE_URL = BASE_URL_PREFIX + ":" + DEFAULT_HTTP_PORT;
-	
 	/**
 	 * The {@link AddressesResource}.
 	 */
@@ -58,34 +41,27 @@ public class AddressBookRestClient
 	/**
 	 * Instantiates a new {@link AddressBookRestClient} with the default base url.
 	 */
-	public AddressBookRestClient() {
+	public AddressBookRestClient()
+	{
 		this(DEFAULT_BASE_URL);
 	}
+
 	/**
 	 * Instantiates a new {@link AddressBookRestClient}.
 	 *
-	 * @param baseUrl the base url
+	 * @param baseUrl
+	 *            the base url
 	 */
-	public AddressBookRestClient(String baseUrl)
+	public AddressBookRestClient(final String baseUrl)
 	{
-		List<Object> providers = new ArrayList<>();
-		providers.add(new JacksonJsonProvider());		
+		super(baseUrl);
+		addressesResource = newResource(AddressesResource.class);
 
-		addressesResource  = JAXRSClientFactory.create(baseUrl, AddressesResource.class, providers);
-        WebClient.client(addressesResource).accept(MediaType.APPLICATION_JSON);
-        WebClient.client(addressesResource).type(MediaType.APPLICATION_JSON);
+		countriesResource = newResource(CountriesResource.class);
 
-        countriesResource  = JAXRSClientFactory.create(baseUrl, CountriesResource.class, providers);
-        WebClient.client(countriesResource).accept(MediaType.APPLICATION_JSON);
-        WebClient.client(countriesResource).type(MediaType.APPLICATION_JSON);
+		federalstatesResource = newResource(FederalstatesResource.class);
 
-        federalstatesResource  = JAXRSClientFactory.create(baseUrl, FederalstatesResource.class, providers);
-        WebClient.client(federalstatesResource).accept(MediaType.APPLICATION_JSON);
-        WebClient.client(federalstatesResource).type(MediaType.APPLICATION_JSON);
-
-        zipcodesResource  = JAXRSClientFactory.create(baseUrl, ZipcodesResource.class, providers);
-        WebClient.client(zipcodesResource).accept(MediaType.APPLICATION_JSON);
-        WebClient.client(zipcodesResource).type(MediaType.APPLICATION_JSON);
+		zipcodesResource =  newResource(ZipcodesResource.class);
 	}
 
 }
