fcdd2c82d1a75c57f83bdae9261fa0eb896ad003
Tatjana Kopalova <tatjana19@users.noreply.github.com>
2016-07-14 16:20:43 +0200

    Update AddressesDao.java

--- address-book-entities/src/main/java/de/alpharogroup/address/book/daos/AddressesDao.java
+++ address-book-entities/src/main/java/de/alpharogroup/address/book/daos/AddressesDao.java
@@ -8,24 +8,17 @@ import org.springframework.stereotype.Repository;
 import de.alpharogroup.address.book.entities.Addresses;
 import de.alpharogroup.db.dao.jpa.JpaEntityManagerDao;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Repository("addressesDao")
 public class AddressesDao extends JpaEntityManagerDao<Addresses, Integer>{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 5693868415897202295L;
 
 	@PersistenceContext
+	@Getter
+	@Setter
 	private EntityManager entityManager;
 
-	@Override
-	public EntityManager getEntityManager() {
-		return entityManager;
-	}
-
-	@Override
-	public void setEntityManager(EntityManager entityManager) {
-		this.entityManager = entityManager;		
-	}
 }
