c0efafb842b4e2f4bce329a02e4b7b58bf83a6b4
Asterios Raptis <asterios.raptis@gmx.net>
2016-07-16 18:28:30 +0200

    Lombokified dao classes

--- address-book-entities/src/main/java/de/alpharogroup/address/book/daos/FederalstatesDao.java
+++ address-book-entities/src/main/java/de/alpharogroup/address/book/daos/FederalstatesDao.java
@@ -7,25 +7,18 @@ import org.springframework.stereotype.Repository;
 
 import de.alpharogroup.address.book.entities.Federalstates;
 import de.alpharogroup.db.dao.jpa.JpaEntityManagerDao;
+import lombok.Getter;
+import lombok.Setter;
 
 @Repository("federalstatesDao")
 public class FederalstatesDao extends JpaEntityManagerDao<Federalstates, Integer>{
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -3937865183860303611L;
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L;
 
+	/** The entity manager. */
 	@PersistenceContext
+	@Getter
+	@Setter
 	private EntityManager entityManager;
-
-	@Override
-	public EntityManager getEntityManager() {
-		return entityManager;
-	}
-
-	@Override
-	public void setEntityManager(EntityManager entityManager) {
-		this.entityManager = entityManager;		
-	}
 }
--- address-book-entities/src/main/java/de/alpharogroup/address/book/daos/ZipcodesDao.java
+++ address-book-entities/src/main/java/de/alpharogroup/address/book/daos/ZipcodesDao.java
@@ -7,25 +7,18 @@ import org.springframework.stereotype.Repository;
 
 import de.alpharogroup.address.book.entities.Zipcodes;
 import de.alpharogroup.db.dao.jpa.JpaEntityManagerDao;
+import lombok.Getter;
+import lombok.Setter;
 
 @Repository("zipcodesDao")
 public class ZipcodesDao extends JpaEntityManagerDao<Zipcodes, Integer>{
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 4820703309882159443L;
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L;
 
+	/** The entity manager. */
 	@PersistenceContext
+	@Getter
+	@Setter
 	private EntityManager entityManager;
-
-	@Override
-	public EntityManager getEntityManager() {
-		return entityManager;
-	}
-
-	@Override
-	public void setEntityManager(EntityManager entityManager) {
-		this.entityManager = entityManager;		
-	}
 }
